3a2b86f9b7d8c5eb59defb468a7b669dca7e418f
githubrohit78 <rohitbtd@gmail.com>
2023-07-17 11:42:44 +0530

    Add project files.

---
+++ JQuerywithMVC/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace JQuerywithMVC.Models
+{
+    public class Customer
+    {
+        public int custid { get; set; }
+        public string Cname { get; set; }
+        public string City { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
